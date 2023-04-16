0f5ca3ad3c00f4c3e4a6482caf2c17940858c874
cristopher <mouravir@gmail.com>
2023-04-16 17:24:37 -0300

    Operadores Aritméticos

--- ex015.py
+++ ex015.py
@@ -1,5 +1,4 @@
-dias = int(input('Quantos dias alugados? '))
-km = float(input('Quantos Km rodados? ' ))
+dias = int(input('Quantos dias alugados?  '))
+km = float(input('Quantos Km rodados?  ' ))
 soma = float((dias*60)+km*0.15)
 print(soma)
-
